ee99892e9f9a14bd4f850a0bf64242ef7f5c4ea2
Rubikoid <rubikoid@rubikoid.ru>
2026-01-29 04:32:27 +0300

    Migrate to working with httpx's URL instead of sucking AnyURL from pydantic

--- aoidc/oauth2/context.py
+++ aoidc/oauth2/context.py
@@ -4,11 +4,11 @@ Helper module for passing validation context
 
 from dataclasses import dataclass, field
 
-from pydantic import AnyUrl
+from httpx import URL
 
 
 @dataclass(init=True, frozen=True, slots=True)
 class ValidationContext:
-    origin_url: AnyUrl
+    origin_url: URL
 
-    allowed_urls: list[AnyUrl] = field(default_factory=list)
+    allowed_urls: list[URL] = field(default_factory=list)
