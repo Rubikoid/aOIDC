c17e0283a1599cdd9bfb7d07684e1a48021902be
Rubikoid <rubikoid@rubikoid.ru>
2026-01-29 04:31:39 +0300

    RFC9864: Update jwt algos

--- aoidc/jwt.py
+++ aoidc/jwt.py
@@ -5,6 +5,8 @@ from enum import StrEnum
 class JsonWebAlgos(StrEnum):
     """
     https://datatracker.ietf.org/doc/html/rfc7518#section-3.1
+
+    + https://datatracker.ietf.org/doc/html/rfc9864#section-2.2
     """
 
     NONE = "none"
@@ -18,9 +20,15 @@ class JsonWebAlgos(StrEnum):
     RS512 = "RS512"
 
     ES256 = "ES256"
+    ES256K = "ES256K"
+
     ES384 = "ES384"
     ES512 = "ES512"
 
     PS256 = "PS256"
     PS384 = "PS384"
     PS512 = "PS512"
+
+    EDDSA = "EdDSA"
+    ED25519 = "Ed25519"
+    ED448 = "Ed448"
